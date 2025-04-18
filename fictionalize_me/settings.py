ad726d7c4c3ec8e5ad1d3df68accedf5ab852b5d
Nick Little <n14little@gmail.com>
2025-04-18 14:21:48 -0500

    chore: add css

--- fictionalize_me/settings.py
+++ fictionalize_me/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Feature Flags
 ENABLE_SIGNUP = os.environ.get('ENABLE_SIGNUP', 'False').lower() == 'true'
