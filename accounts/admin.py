1701fca2cc329a35b0db1cc9a50691208691b08f
Nick Little <n14little@gmail.com>
2025-04-22 05:23:37 -0500

    chore: remove django app

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import WaitlistEntry
-
-@admin.register(WaitlistEntry)
-class WaitlistEntryAdmin(admin.ModelAdmin):
-    list_display = ('email', 'created_at')
-    search_fields = ('email',)
-    ordering = ('-created_at',)
--- features/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .services import FeatureService
-
-def feature_flags(request):
-    """Make all feature flags available in templates."""
-    return {
-        'features': FeatureService.get_all_features()
-    }
